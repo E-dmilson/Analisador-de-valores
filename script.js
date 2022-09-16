7e4068b3a8d31423a94d7cf7f7ea9665f7948731
Edmilson Alves <edmilsonnhamposse@gmail.com>
2022-09-16 09:12:02 +0100

    Ex018 do curso em video de JS para prática de vetores e funções

---
+++ script.js
@@ -0,0 +1,62 @@
+let numero = document.getElementById('num')
+let tab = document.getElementById('tabela')
+let res = document.getElementById('ans')
+let valores = []
+
+let clk = document.getElementById('add').addEventListener('click', adicionar)
+let final = document.getElementById('fin').addEventListener('click', finalizar)
+function isNumero(n){
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
+    }else{
+        return false
+    }
+}
+
+function inLista(n, l){
+    if(l.indexOf(Number(n)) != -1){
+        return true
+    }else{
+        return false
+    }
+}
+
+function adicionar(){       
+    if(isNumero(numero.value) && !inLista(numero.value, valores)){
+        valores.push(Number(numero.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${numero.value} adicionado`
+        tab.appendChild(item) 
+        res.innerHTML = ''
+    }else{
+        alert('Valor invalido ou ja encontrado na lista')
+    }
+    numero.value = ''
+    numero.focus()
+}
+
+function finalizar(){
+    if(valores.length == 0){
+        alert('Adicione valores antes de finalizar')
+    }else{
+        let total = valores.length
+        let maior = valores[0] 
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores){
+            soma += valores[pos]
+            if(valores[0] > maior)
+                maior = valores[pos]
+            if(valores[pos] < menor)
+            menor = valores[pos]
+        }
+        media = soma/total
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${total} numeros cadastrados.</>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.</>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}.</>`
+        res.innerHTML += `<p>Somando todos os valores temos ${soma}.</>`
+        res.innerHTML += `<p>A media dos valores digitados e ${media}.</>`
+    }
+}
